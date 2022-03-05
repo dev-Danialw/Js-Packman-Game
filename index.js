2b0d80dd021b99e52c79dfe49fcb97d4faf44722
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-05 21:29:37 +0500

    Displayed Layout

--- index.js
+++ index.js
@@ -52,6 +52,16 @@ function createBoard() {
     grid.appendChild(square);
     // putting squares in the squares array
     squares.push(square);
+
+    if (layout[i] === 0) {
+      squares[i].classList.add("pac-dot");
+    }
+    if (layout[i] === 1) {
+      squares[i].classList.add("wall");
+    }
+    if (layout[i] == 3) {
+      squares[i].classList.add("power-pellet");
+    }
   }
 }
 createBoard();
