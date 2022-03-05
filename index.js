808e3be84f7c8aade6b7f46cc943be6588c09b63
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-05 21:39:21 +0500

    Added a switch statement for moving the Pacman

--- index.js
+++ index.js
@@ -67,6 +67,28 @@ function createBoard() {
 createBoard();
 
 // starting position of Pacman
-
 let pacmanCurrentIndex = 490;
 squares[pacmanCurrentIndex].classList.add("pacman");
+
+// Moving the pacman
+
+function control(e) {
+  switch (e.keyCode) {
+    case 40:
+      console.log("down");
+      break;
+    case 38:
+      console.log("up");
+      break;
+    case 37:
+      console.log("left");
+      break;
+    case 39:
+      console.log("right");
+      break;
+
+    default:
+      break;
+  }
+}
+document.addEventListener("keyup", control);
